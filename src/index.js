853476297573b39cc946176d74ff55d572e3d92d
mo7mad4 <mohammedreda814@gmail.com>
2023-08-11 21:09:07 +0300

    Link router to app js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,15 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
+<React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <ThemeProvider>
+          <AlertProvider>
+            <App />
+          </AlertProvider>
+        </ThemeProvider>
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
