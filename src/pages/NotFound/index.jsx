4a92f81d806f85ada74d07e66731b3ce808b3e84
mo7mad4 <mohammedreda814@gmail.com>
2023-09-23 17:46:57 +0300

    Fix notfound page and add image

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 import './style.css';
 import { Link } from 'react-router-dom';
 import { PATHS } from '../../router/pathes';
-
+// import notfoundIamge from '../../assets/notfound.jpg';
 const NotFound = () => {
   return (
     <div className='container'>
-      <h1 className='error-code'>404</h1>
-      <p className='error-message'>Oops! Page not found</p>
+      <img src='assets/notfound.jpg' alt="" srcset="" />
       <p>
-        Go back to{' '}
-        <Link to={PATHS.HOME} className='back-link'>
-          home
-        </Link>
+        <button>
+          <Link to={PATHS.HOME} className='back-link'>
+            Go to Home
+          </Link>
+        </button>
         .
       </p>
     </div>
